9921b5279c940ecb8ba21cc2b00064a783bafda7
Iain Anderson <iain@iotechsys.com>
2019-04-23 13:59:21 +0100

    F-55 doc update
    Signed-off-by: Iain Anderson <iain@iotechsys.com>

--- include/edgex/eventgen.h
+++ include/edgex/eventgen.h
@@ -39,6 +39,23 @@ void edgex_device_post_readings
   const edgex_device_commandresult *values
 );
 
+/**
+ * @brief Callback function requesting that automatic events should begin. 
+ *        These should be generated according to the schedule given, and posted
+ *        into EdgeX using edgex_device_post_readings().
+ * @param impl The context data passed in when the service was created.
+ * @param devname The name of the device to be queried.
+ * @param protocols The location of the device to be queried.
+ * @param resource_name The resource on which autoevents have been configured.
+ * @param nreadings The number of readings requested.
+ * @param requests An array specifying the readings that have been requested.
+ * @param interval The time between events, in milliseconds.
+ * @param onChange If true, events should only be generated if one or more
+ *        readings have changed.
+ * @return A pointer to a data structure that will be provided in a subsequent
+ *         call to the stop handler.
+ */
+
 typedef void * (*edgex_device_autoevent_start_handler)
 (
   void *impl,
@@ -51,11 +68,28 @@ typedef void * (*edgex_device_autoevent_start_handler)
   bool onChange
 );
 
+/**
+ * @brief Callback function requesting that automatic events should cease.
+ * @param impl The context data passed in when the service was created.
+ * @param handle The data structure returned by a previous call to the start
+ *        handler.
+ */
+
 typedef void (*edgex_device_autoevent_stop_handler)
 (
   void *impl,
   void *handle
 );
+
+/**
+ * @brief This function allows a device service implementation to provide its
+ *        own mechanism for generating automatic events. This may be useful
+ *        in scenarios where the hardware can be configured to push readings on
+ *        a schedule. Handlers must be registerd before starting the service.
+ * @param svc The device service.
+ * @param starter Function to handle requests to start generating events.
+ * @param stopper Function to handle requests to stop generating events.
+ */
 
 void edgex_device_register_autoevent_handlers
 (
