a8b02f2e44090a35dbb33b28118c13f29bf9e3df
Iain Anderson <iain@iotechsys.com>
2019-09-17 10:32:50 +0100

    Move docker build to Alpine 3.9 Use released version of CBOR library Declutter the output of the docker build
    Signed-off-by: Iain Anderson <iain@iotechsys.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -x -e
 
+CBOR_VERSION=0.5.0
+
 mkdir /deps
 cd /deps
-git clone https://github.com/PJK/libcbor
-sed -e 's/-flto//' -i libcbor/CMakeLists.txt
-cmake -DCMAKE_BUILD_TYPE=Release -DCBOR_CUSTOM_ALLOC=ON libcbor
+wget -O - https://github.com/PJK/libcbor/archive/v${CBOR_VERSION}.tar.gz | tar -z -x -f -
+sed -e 's/-flto//' -i libcbor-${CBOR_VERSION}/CMakeLists.txt
+cmake -DCMAKE_BUILD_TYPE=Release -DCBOR_CUSTOM_ALLOC=ON libcbor-${CBOR_VERSION}
 make
 make install
 
@@ -13,6 +15,11 @@ make install
 
 /edgex-c-sdk/scripts/build.sh $*
 
+mkdir -p /edgex-c-sdk/results/debug
+cp /edgex-c-sdk/build/debug/c/libcsdk.so /edgex-c-sdk/results/debug
+cp /edgex-c-sdk/build/release/csdk-*.tar.gz /edgex-c-sdk/results
+cp /edgex-c-sdk/build/release/release.log /edgex-c-sdk/results
+
 # Set ownership of generated files
 
-chown -R $UID:$GID /edgex-c-sdk/build
+chown -R $UID:$GID /edgex-c-sdk/results
