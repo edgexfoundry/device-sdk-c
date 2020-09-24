251b62f986d85ae3dd54b3106bd7c0f4b32dca36
Iain Anderson <iain@iotechsys.com>
2020-09-24 13:35:00 +0100

    fix: url-escape device names when setting opstate. Make opstate setting part of the devsdk interface.
    Signed-off-by: Iain Anderson <iain@iotechsys.com>

--- src/c/devman.c
+++ src/c/devman.c
@@ -229,7 +229,7 @@ void devsdk_add_discovered_devices (devsdk_service_t *svc, uint32_t ndevices, de
   }
 }
 
-void edgex_set_device_opstate (devsdk_service_t *svc, char *devname, bool operational, devsdk_error *err)
+void devsdk_set_device_opstate (devsdk_service_t *svc, char *devname, bool operational, devsdk_error *err)
 {
   *err = EDGEX_OK;
   edgex_metadata_client_set_device_opstate_byname
